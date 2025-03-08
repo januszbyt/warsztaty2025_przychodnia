6893d9c3b9791b75606a263705a1d1b8642fe4ec
s41145 <s41145@C45-K06>
2025-03-08 10:28:47 +0100

    Andrzej Antosz

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -25,7 +25,7 @@ namespace WindowsFormsApp1
         private void button2_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Witaj 1");
-
+            //xdxdxdx
         }
 
         private void button3_Click(object sender, EventArgs e)
