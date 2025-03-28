582de9b7719dfe189a687127dbcf71595da6e7f5
Andrzej Antosz <s41145@s.pansjar.edu.pl>
2025-03-29 00:59:29 +0100

    Dodanie opcje wybrania Rol dla lekarza, pacjenta i admina, oraz formularze logowania dla odpowiednich person, oraz dodany zostal zarys jak ma wygladac formularz panelu lekarza, oczywiscie do przekonsultowania z grupa. Zostalo to zrobione przez Andrzej Antosz oraz Piotr Szymczycha. Piotr zajął sie projektowaniem formularza ja zaimpelentowalem kod dla formularzy. Wysyłam tylko ja botrzeba przestestowac dzialanie githuba.

---
+++ WindowsFormsApp1/FormLogowanieAdmin.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FormLogowanieAdmin : Form
+    {
+        public FormLogowanieAdmin()
+        {
+            InitializeComponent();
+            textBoxHasloAdmin.PasswordChar = '•';
+
+            textBoxLoginAdmin.Text = "Admin";
+            textBoxHasloAdmin.Focus();
+        }
+
+        private void textBoxLoginAdmin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxHasloAdmin_TextChanged(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(sender, e);
+            }   
+        }
+
+        private void checkBoxHasloAdmin_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxHasloAdmin.PasswordChar = checkBoxHasloAdmin.Checked ? '\0' : '•';
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           string login = textBoxLoginAdmin.Text.Trim();
+            string haslo = textBoxHasloAdmin.Text;
+
+            if (login.Equals("Admin", StringComparison.OrdinalIgnoreCase) && haslo == "PANS")
+            {
+                MessageBox.Show("Logowanie udane, Witaj Administratorze !","Sukces", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                this.Hide();
+                FormLogowanieAdmin panelAdmin = new FormLogowanieAdmin();
+                panelAdmin.ShowDialog();
+                this.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Nieprawidłowy login lub hasło!", "Błąd logowania",
+                              MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                textBoxHasloAdmin.Text = "";
+                textBoxHasloAdmin.Focus();
+            }
+
+        }
+    }
+}
