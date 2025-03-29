f9908b32e90cb6256c0d69abe59e66e143e7fdb8
Kamil Fedan <s40851@s.pansjar.edu.pl>
2025-03-29 16:20:24 +0100

    baza danych

--- WindowsFormsApp1/FormLogowanieAdmin.Designer.cs
+++ WindowsFormsApp1/FormLogowanieAdmin.Designer.cs
@@ -41,10 +41,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(140, 170);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Location = new System.Drawing.Point(123, 137);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 2;
             this.button1.Text = "Zaloguj";
             this.button1.UseVisualStyleBackColor = true;
@@ -52,28 +52,28 @@
             // 
             // textBoxLoginAdmin
             // 
-            this.textBoxLoginAdmin.Location = new System.Drawing.Point(126, 61);
-            this.textBoxLoginAdmin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxLoginAdmin.Location = new System.Drawing.Point(112, 49);
+            this.textBoxLoginAdmin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxLoginAdmin.Name = "textBoxLoginAdmin";
-            this.textBoxLoginAdmin.Size = new System.Drawing.Size(148, 26);
+            this.textBoxLoginAdmin.Size = new System.Drawing.Size(132, 22);
             this.textBoxLoginAdmin.TabIndex = 3;
             this.textBoxLoginAdmin.TextChanged += new System.EventHandler(this.textBoxLoginAdmin_TextChanged);
             // 
             // textBoxHasloAdmin
             // 
-            this.textBoxHasloAdmin.Location = new System.Drawing.Point(126, 102);
-            this.textBoxHasloAdmin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxHasloAdmin.Location = new System.Drawing.Point(112, 82);
+            this.textBoxHasloAdmin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxHasloAdmin.Name = "textBoxHasloAdmin";
-            this.textBoxHasloAdmin.Size = new System.Drawing.Size(148, 26);
+            this.textBoxHasloAdmin.Size = new System.Drawing.Size(132, 22);
             this.textBoxHasloAdmin.TabIndex = 4;
             // 
             // checkBoxHasloAdmin
             // 
             this.checkBoxHasloAdmin.AutoSize = true;
-            this.checkBoxHasloAdmin.Location = new System.Drawing.Point(140, 136);
-            this.checkBoxHasloAdmin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBoxHasloAdmin.Location = new System.Drawing.Point(124, 109);
+            this.checkBoxHasloAdmin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxHasloAdmin.Name = "checkBoxHasloAdmin";
-            this.checkBoxHasloAdmin.Size = new System.Drawing.Size(122, 24);
+            this.checkBoxHasloAdmin.Size = new System.Drawing.Size(106, 20);
             this.checkBoxHasloAdmin.TabIndex = 5;
             this.checkBoxHasloAdmin.Text = "Pokaż hasło";
             this.checkBoxHasloAdmin.UseVisualStyleBackColor = true;
@@ -83,10 +83,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(48, 21);
+            this.label1.Location = new System.Drawing.Point(43, 17);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(217, 25);
+            this.label1.Size = new System.Drawing.Size(180, 20);
             this.label1.TabIndex = 6;
             this.label1.Text = "WITAMY W KLINICE";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -96,9 +96,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label2.Location = new System.Drawing.Point(35, 64);
+            this.label2.Location = new System.Drawing.Point(31, 51);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.Size = new System.Drawing.Size(64, 17);
             this.label2.TabIndex = 8;
             this.label2.Text = "E-MAIL:";
             // 
@@ -107,36 +107,36 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label3.Location = new System.Drawing.Point(35, 102);
+            this.label3.Location = new System.Drawing.Point(31, 82);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.Size = new System.Drawing.Size(65, 17);
             this.label3.TabIndex = 12;
             this.label3.Text = "HASŁO:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(24, 223);
+            this.label5.Location = new System.Drawing.Point(21, 178);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(276, 20);
+            this.label5.Size = new System.Drawing.Size(232, 16);
             this.label5.TabIndex = 18;
             this.label5.Text = "PONOWNY WYBÓR UŻYTKOWNIKA";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(79, 248);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Location = new System.Drawing.Point(70, 198);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(161, 57);
+            this.button3.Size = new System.Drawing.Size(143, 46);
             this.button3.TabIndex = 19;
             this.button3.Text = "<< COFNIJ";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // FormLogowanieAdmin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(338, 344);
+            this.ClientSize = new System.Drawing.Size(300, 275);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label3);
@@ -146,7 +146,7 @@
             this.Controls.Add(this.textBoxHasloAdmin);
             this.Controls.Add(this.textBoxLoginAdmin);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormLogowanieAdmin";
             this.Text = "Logowanie Admin";
             this.ResumeLayout(false);
