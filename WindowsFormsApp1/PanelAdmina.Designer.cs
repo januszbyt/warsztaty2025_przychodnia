28d93377ac7a6343016b3faf841f24a02651b21a
User <User@LAPTOP>
2025-05-10 10:57:48 +0200

    zmiana panelu admin

--- WindowsFormsApp1/PanelAdmina.Designer.cs
+++ WindowsFormsApp1/PanelAdmina.Designer.cs
@@ -57,10 +57,9 @@
             // 
             // buttonWyswierlLekarzy
             // 
-            this.buttonWyswierlLekarzy.Location = new System.Drawing.Point(16, 15);
-            this.buttonWyswierlLekarzy.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonWyswierlLekarzy.Location = new System.Drawing.Point(12, 12);
             this.buttonWyswierlLekarzy.Name = "buttonWyswierlLekarzy";
-            this.buttonWyswierlLekarzy.Size = new System.Drawing.Size(169, 59);
+            this.buttonWyswierlLekarzy.Size = new System.Drawing.Size(127, 48);
             this.buttonWyswierlLekarzy.TabIndex = 0;
             this.buttonWyswierlLekarzy.Text = "Wyswietl Lekarzy";
             this.buttonWyswierlLekarzy.UseVisualStyleBackColor = true;
@@ -68,10 +67,9 @@
             // 
             // buttonWyswietlPacjentow
             // 
-            this.buttonWyswietlPacjentow.Location = new System.Drawing.Point(16, 81);
-            this.buttonWyswietlPacjentow.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonWyswietlPacjentow.Location = new System.Drawing.Point(12, 66);
             this.buttonWyswietlPacjentow.Name = "buttonWyswietlPacjentow";
-            this.buttonWyswietlPacjentow.Size = new System.Drawing.Size(169, 59);
+            this.buttonWyswietlPacjentow.Size = new System.Drawing.Size(127, 48);
             this.buttonWyswietlPacjentow.TabIndex = 1;
             this.buttonWyswietlPacjentow.Text = "Wyświetl Pacjentow";
             this.buttonWyswietlPacjentow.UseVisualStyleBackColor = true;
@@ -79,10 +77,9 @@
             // 
             // buttonNadajUprawienia
             // 
-            this.buttonNadajUprawienia.Location = new System.Drawing.Point(16, 148);
-            this.buttonNadajUprawienia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonNadajUprawienia.Location = new System.Drawing.Point(12, 120);
             this.buttonNadajUprawienia.Name = "buttonNadajUprawienia";
-            this.buttonNadajUprawienia.Size = new System.Drawing.Size(169, 59);
+            this.buttonNadajUprawienia.Size = new System.Drawing.Size(127, 48);
             this.buttonNadajUprawienia.TabIndex = 2;
             this.buttonNadajUprawienia.Text = "Nadaj Uprawnienia";
             this.buttonNadajUprawienia.UseVisualStyleBackColor = true;
@@ -90,10 +87,9 @@
             // 
             // buttonZabierzUprawnienia
             // 
-            this.buttonZabierzUprawnienia.Location = new System.Drawing.Point(16, 214);
-            this.buttonZabierzUprawnienia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonZabierzUprawnienia.Location = new System.Drawing.Point(12, 174);
             this.buttonZabierzUprawnienia.Name = "buttonZabierzUprawnienia";
-            this.buttonZabierzUprawnienia.Size = new System.Drawing.Size(169, 59);
+            this.buttonZabierzUprawnienia.Size = new System.Drawing.Size(127, 48);
             this.buttonZabierzUprawnienia.TabIndex = 3;
             this.buttonZabierzUprawnienia.Text = "Zabierz Uprawnienia";
             this.buttonZabierzUprawnienia.UseVisualStyleBackColor = true;
@@ -101,10 +97,9 @@
             // 
             // buttonUsunRekordy
             // 
-            this.buttonUsunRekordy.Location = new System.Drawing.Point(16, 281);
-            this.buttonUsunRekordy.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonUsunRekordy.Location = new System.Drawing.Point(12, 228);
             this.buttonUsunRekordy.Name = "buttonUsunRekordy";
-            this.buttonUsunRekordy.Size = new System.Drawing.Size(169, 59);
+            this.buttonUsunRekordy.Size = new System.Drawing.Size(127, 48);
             this.buttonUsunRekordy.TabIndex = 4;
             this.buttonUsunRekordy.Text = "Usun Uzytkownika";
             this.buttonUsunRekordy.UseVisualStyleBackColor = true;
@@ -112,10 +107,9 @@
             // 
             // buttonDodajLekarza
             // 
-            this.buttonDodajLekarza.Location = new System.Drawing.Point(1703, 641);
-            this.buttonDodajLekarza.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonDodajLekarza.Location = new System.Drawing.Point(1277, 521);
             this.buttonDodajLekarza.Name = "buttonDodajLekarza";
-            this.buttonDodajLekarza.Size = new System.Drawing.Size(169, 59);
+            this.buttonDodajLekarza.Size = new System.Drawing.Size(127, 48);
             this.buttonDodajLekarza.TabIndex = 5;
             this.buttonDodajLekarza.Text = "Dodaj Lekarza";
             this.buttonDodajLekarza.UseVisualStyleBackColor = true;
@@ -123,10 +117,9 @@
             // 
             // buttonFormDodaniaLekarza
             // 
-            this.buttonFormDodaniaLekarza.Location = new System.Drawing.Point(16, 347);
-            this.buttonFormDodaniaLekarza.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonFormDodaniaLekarza.Location = new System.Drawing.Point(12, 282);
             this.buttonFormDodaniaLekarza.Name = "buttonFormDodaniaLekarza";
-            this.buttonFormDodaniaLekarza.Size = new System.Drawing.Size(169, 59);
+            this.buttonFormDodaniaLekarza.Size = new System.Drawing.Size(127, 48);
             this.buttonFormDodaniaLekarza.TabIndex = 6;
             this.buttonFormDodaniaLekarza.Text = "Formularz dodania Lekarza";
             this.buttonFormDodaniaLekarza.UseVisualStyleBackColor = true;
@@ -135,10 +128,9 @@
             // buttonUsunWszystko
             // 
             this.buttonUsunWszystko.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.buttonUsunWszystko.Location = new System.Drawing.Point(16, 414);
-            this.buttonUsunWszystko.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonUsunWszystko.Location = new System.Drawing.Point(12, 369);
             this.buttonUsunWszystko.Name = "buttonUsunWszystko";
-            this.buttonUsunWszystko.Size = new System.Drawing.Size(169, 59);
+            this.buttonUsunWszystko.Size = new System.Drawing.Size(127, 48);
             this.buttonUsunWszystko.TabIndex = 7;
             this.buttonUsunWszystko.Text = "Usun wszystko z bazy";
             this.buttonUsunWszystko.UseVisualStyleBackColor = true;
@@ -147,19 +139,17 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(203, 15);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(152, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(1209, 842);
+            this.dataGridView1.Size = new System.Drawing.Size(907, 684);
             this.dataGridView1.TabIndex = 14;
             // 
             // WylogujAdmin
             // 
-            this.WylogujAdmin.Location = new System.Drawing.Point(1823, 15);
-            this.WylogujAdmin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WylogujAdmin.Location = new System.Drawing.Point(1367, 12);
             this.WylogujAdmin.Name = "WylogujAdmin";
-            this.WylogujAdmin.Size = new System.Drawing.Size(156, 59);
+            this.WylogujAdmin.Size = new System.Drawing.Size(117, 48);
             this.WylogujAdmin.TabIndex = 15;
             this.WylogujAdmin.Text = "Wyloguj";
             this.WylogujAdmin.UseVisualStyleBackColor = true;
@@ -167,45 +157,40 @@
             // 
             // buttonPokaz
             // 
-            this.buttonPokaz.Location = new System.Drawing.Point(1516, 15);
-            this.buttonPokaz.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonPokaz.Location = new System.Drawing.Point(1137, 12);
             this.buttonPokaz.Name = "buttonPokaz";
-            this.buttonPokaz.Size = new System.Drawing.Size(79, 36);
+            this.buttonPokaz.Size = new System.Drawing.Size(59, 29);
             this.buttonPokaz.TabIndex = 16;
             this.buttonPokaz.Text = "Pokaż";
             this.buttonPokaz.UseVisualStyleBackColor = true;
             // 
             // textBoxImie
             // 
-            this.textBoxImie.Location = new System.Drawing.Point(1701, 466);
-            this.textBoxImie.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxImie.Location = new System.Drawing.Point(1276, 379);
             this.textBoxImie.Name = "textBoxImie";
-            this.textBoxImie.Size = new System.Drawing.Size(169, 22);
+            this.textBoxImie.Size = new System.Drawing.Size(128, 20);
             this.textBoxImie.TabIndex = 19;
             // 
             // textBoxNazwisko
             // 
-            this.textBoxNazwisko.Location = new System.Drawing.Point(1701, 498);
-            this.textBoxNazwisko.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxNazwisko.Location = new System.Drawing.Point(1276, 405);
             this.textBoxNazwisko.Name = "textBoxNazwisko";
-            this.textBoxNazwisko.Size = new System.Drawing.Size(169, 22);
+            this.textBoxNazwisko.Size = new System.Drawing.Size(128, 20);
             this.textBoxNazwisko.TabIndex = 20;
             // 
             // textBoxSpecjalizacja
             // 
-            this.textBoxSpecjalizacja.Location = new System.Drawing.Point(1701, 530);
-            this.textBoxSpecjalizacja.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxSpecjalizacja.Location = new System.Drawing.Point(1276, 431);
             this.textBoxSpecjalizacja.Name = "textBoxSpecjalizacja";
-            this.textBoxSpecjalizacja.Size = new System.Drawing.Size(169, 22);
+            this.textBoxSpecjalizacja.Size = new System.Drawing.Size(128, 20);
             this.textBoxSpecjalizacja.TabIndex = 21;
             // 
             // radioButtonLekarze
             // 
             this.radioButtonLekarze.AutoSize = true;
-            this.radioButtonLekarze.Location = new System.Drawing.Point(1519, 281);
-            this.radioButtonLekarze.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonLekarze.Location = new System.Drawing.Point(1139, 228);
             this.radioButtonLekarze.Name = "radioButtonLekarze";
-            this.radioButtonLekarze.Size = new System.Drawing.Size(76, 20);
+            this.radioButtonLekarze.Size = new System.Drawing.Size(63, 17);
             this.radioButtonLekarze.TabIndex = 22;
             this.radioButtonLekarze.TabStop = true;
             this.radioButtonLekarze.Text = "Lekarze";
@@ -214,10 +199,9 @@
             // radioButtonPacjenci
             // 
             this.radioButtonPacjenci.AutoSize = true;
-            this.radioButtonPacjenci.Location = new System.Drawing.Point(1519, 319);
-            this.radioButtonPacjenci.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonPacjenci.Location = new System.Drawing.Point(1139, 259);
             this.radioButtonPacjenci.Name = "radioButtonPacjenci";
-            this.radioButtonPacjenci.Size = new System.Drawing.Size(80, 20);
+            this.radioButtonPacjenci.Size = new System.Drawing.Size(66, 17);
             this.radioButtonPacjenci.TabIndex = 23;
             this.radioButtonPacjenci.TabStop = true;
             this.radioButtonPacjenci.Text = "Pacjenci";
@@ -226,82 +210,74 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(1616, 470);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(1212, 382);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(33, 16);
+            this.label5.Size = new System.Drawing.Size(29, 13);
             this.label5.TabIndex = 25;
             this.label5.Text = "IMIE";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1616, 502);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(1212, 408);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.Size = new System.Drawing.Size(65, 13);
             this.label3.TabIndex = 26;
             this.label3.Text = "NAZWISKO";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(1575, 534);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(1181, 434);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(111, 16);
+            this.label4.Size = new System.Drawing.Size(89, 13);
             this.label4.TabIndex = 27;
             this.label4.Text = "SPECJALIZACJA";
             // 
             // textBoxHaslo
             // 
-            this.textBoxHaslo.Location = new System.Drawing.Point(1701, 597);
-            this.textBoxHaslo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxHaslo.Location = new System.Drawing.Point(1276, 485);
             this.textBoxHaslo.Name = "textBoxHaslo";
-            this.textBoxHaslo.Size = new System.Drawing.Size(169, 22);
+            this.textBoxHaslo.Size = new System.Drawing.Size(128, 20);
             this.textBoxHaslo.TabIndex = 28;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1631, 606);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(1223, 492);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.Size = new System.Drawing.Size(43, 13);
             this.label2.TabIndex = 29;
             this.label2.Text = "HASLO";
             // 
             // textBoxEmail
             // 
-            this.textBoxEmail.Location = new System.Drawing.Point(1701, 565);
-            this.textBoxEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxEmail.Location = new System.Drawing.Point(1276, 459);
             this.textBoxEmail.Name = "textBoxEmail";
-            this.textBoxEmail.Size = new System.Drawing.Size(169, 22);
+            this.textBoxEmail.Size = new System.Drawing.Size(128, 20);
             this.textBoxEmail.TabIndex = 30;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1631, 574);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(1223, 466);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.Size = new System.Drawing.Size(32, 13);
             this.label1.TabIndex = 31;
             this.label1.Text = "Email";
             // 
             // textBoxSpecjalizacjaUprawnienia
             // 
-            this.textBoxSpecjalizacjaUprawnienia.Location = new System.Drawing.Point(1600, 796);
-            this.textBoxSpecjalizacjaUprawnienia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxSpecjalizacjaUprawnienia.Location = new System.Drawing.Point(1200, 647);
             this.textBoxSpecjalizacjaUprawnienia.Name = "textBoxSpecjalizacjaUprawnienia";
-            this.textBoxSpecjalizacjaUprawnienia.Size = new System.Drawing.Size(212, 22);
+            this.textBoxSpecjalizacjaUprawnienia.Size = new System.Drawing.Size(160, 20);
             this.textBoxSpecjalizacjaUprawnienia.TabIndex = 32;
             // 
             // PanelAdmina
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1924, 1022);
+            this.ClientSize = new System.Drawing.Size(1028, 609);
             this.Controls.Add(this.textBoxSpecjalizacjaUprawnienia);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxEmail);
@@ -326,7 +302,6 @@
             this.Controls.Add(this.buttonNadajUprawienia);
             this.Controls.Add(this.buttonWyswietlPacjentow);
             this.Controls.Add(this.buttonWyswierlLekarzy);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PanelAdmina";
             this.Text = "PanelAdmina";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
