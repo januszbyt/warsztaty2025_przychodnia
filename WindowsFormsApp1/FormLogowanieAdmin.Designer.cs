1cf35c370719671248bbb6d393cbccd89224f955
Andrzej <Andrzej@DESKTOP-HKDBQIC>
2025-05-24 15:44:47 +0200

    kolejne zmiany czcionki dla calego projektu

--- WindowsFormsApp1/FormLogowanieAdmin.Designer.cs
+++ WindowsFormsApp1/FormLogowanieAdmin.Designer.cs
@@ -39,10 +39,10 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Silver;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(372, 77);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(418, 96);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(143, 42);
+            this.button1.Size = new System.Drawing.Size(161, 52);
             this.button1.TabIndex = 2;
             this.button1.Text = "Zaloguj";
             this.button1.UseVisualStyleBackColor = false;
@@ -50,29 +50,30 @@
             // 
             // textBoxLoginAdmin
             // 
-            this.textBoxLoginAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBoxLoginAdmin.Location = new System.Drawing.Point(128, 69);
-            this.textBoxLoginAdmin.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxLoginAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxLoginAdmin.Location = new System.Drawing.Point(144, 86);
+            this.textBoxLoginAdmin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxLoginAdmin.Name = "textBoxLoginAdmin";
-            this.textBoxLoginAdmin.Size = new System.Drawing.Size(205, 37);
+            this.textBoxLoginAdmin.Size = new System.Drawing.Size(230, 44);
             this.textBoxLoginAdmin.TabIndex = 5;
+            this.textBoxLoginAdmin.TextChanged += new System.EventHandler(this.textBoxLoginAdmin_TextChanged);
             // 
             // textBoxHasloAdmin
             // 
-            this.textBoxHasloAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBoxHasloAdmin.Location = new System.Drawing.Point(128, 127);
-            this.textBoxHasloAdmin.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxHasloAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxHasloAdmin.Location = new System.Drawing.Point(144, 159);
+            this.textBoxHasloAdmin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxHasloAdmin.Name = "textBoxHasloAdmin";
-            this.textBoxHasloAdmin.Size = new System.Drawing.Size(205, 37);
+            this.textBoxHasloAdmin.Size = new System.Drawing.Size(230, 44);
             this.textBoxHasloAdmin.TabIndex = 4;
             // 
             // checkBoxHasloAdmin
             // 
             this.checkBoxHasloAdmin.AutoSize = true;
-            this.checkBoxHasloAdmin.Location = new System.Drawing.Point(372, 127);
-            this.checkBoxHasloAdmin.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBoxHasloAdmin.Location = new System.Drawing.Point(418, 159);
+            this.checkBoxHasloAdmin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.checkBoxHasloAdmin.Name = "checkBoxHasloAdmin";
-            this.checkBoxHasloAdmin.Size = new System.Drawing.Size(106, 20);
+            this.checkBoxHasloAdmin.Size = new System.Drawing.Size(122, 24);
             this.checkBoxHasloAdmin.TabIndex = 5;
             this.checkBoxHasloAdmin.Text = "Pokaż hasło";
             this.checkBoxHasloAdmin.UseVisualStyleBackColor = true;
@@ -83,10 +84,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(142, 9);
+            this.label1.Location = new System.Drawing.Point(160, 11);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(289, 32);
+            this.label1.Size = new System.Drawing.Size(338, 38);
             this.label1.TabIndex = 6;
             this.label1.Text = "WITAMY W KLINICE";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -97,9 +98,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label2.Location = new System.Drawing.Point(29, 77);
+            this.label2.Location = new System.Drawing.Point(33, 96);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 25);
+            this.label2.Size = new System.Drawing.Size(104, 29);
             this.label2.TabIndex = 8;
             this.label2.Text = "E-MAIL:";
             // 
@@ -108,9 +109,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label3.Location = new System.Drawing.Point(29, 133);
+            this.label3.Location = new System.Drawing.Point(33, 166);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 25);
+            this.label3.Size = new System.Drawing.Size(107, 29);
             this.label3.TabIndex = 12;
             this.label3.Text = "HASŁO:";
             // 
@@ -118,9 +119,9 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.Location = new System.Drawing.Point(118, 244);
+            this.label5.Location = new System.Drawing.Point(133, 305);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(313, 20);
+            this.label5.Size = new System.Drawing.Size(394, 25);
             this.label5.TabIndex = 18;
             this.label5.Text = "PONOWNY WYBÓR UŻYTKOWNIKA";
             // 
@@ -128,10 +129,10 @@
             // 
             this.button3.BackColor = System.Drawing.Color.Silver;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button3.Location = new System.Drawing.Point(199, 282);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(224, 352);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(143, 46);
+            this.button3.Size = new System.Drawing.Size(161, 58);
             this.button3.TabIndex = 19;
             this.button3.Text = "<< COFNIJ";
             this.button3.UseVisualStyleBackColor = false;
@@ -139,10 +140,10 @@
             // 
             // FormLogowanieAdmin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(563, 462);
+            this.ClientSize = new System.Drawing.Size(633, 578);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label3);
@@ -152,7 +153,7 @@
             this.Controls.Add(this.textBoxHasloAdmin);
             this.Controls.Add(this.textBoxLoginAdmin);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FormLogowanieAdmin";
             this.Text = "Logowanie Admin";
             this.Load += new System.EventHandler(this.FormLogowanieAdmin_Load);
--- WindowsFormsApp1/FormLogowaniePacjent.Designer.cs
+++ WindowsFormsApp1/FormLogowaniePacjent.Designer.cs
@@ -45,29 +45,31 @@
             // 
             // textBoxLoginPacjent
             // 
-            this.textBoxLoginPacjent.Location = new System.Drawing.Point(145, 104);
-            this.textBoxLoginPacjent.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxLoginPacjent.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxLoginPacjent.Location = new System.Drawing.Point(163, 130);
+            this.textBoxLoginPacjent.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxLoginPacjent.Multiline = true;
             this.textBoxLoginPacjent.Name = "textBoxLoginPacjent";
-            this.textBoxLoginPacjent.Size = new System.Drawing.Size(194, 39);
+            this.textBoxLoginPacjent.Size = new System.Drawing.Size(218, 48);
             this.textBoxLoginPacjent.TabIndex = 4;
             // 
             // textBoxHasloPacjent
             // 
-            this.textBoxHasloPacjent.Location = new System.Drawing.Point(145, 177);
-            this.textBoxHasloPacjent.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxHasloPacjent.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxHasloPacjent.Location = new System.Drawing.Point(163, 221);
+            this.textBoxHasloPacjent.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxHasloPacjent.Multiline = true;
             this.textBoxHasloPacjent.Name = "textBoxHasloPacjent";
-            this.textBoxHasloPacjent.Size = new System.Drawing.Size(194, 39);
+            this.textBoxHasloPacjent.Size = new System.Drawing.Size(218, 48);
             this.textBoxHasloPacjent.TabIndex = 5;
             // 
             // checkBoxPokazHaslo
             // 
             this.checkBoxPokazHaslo.AutoSize = true;
-            this.checkBoxPokazHaslo.Location = new System.Drawing.Point(501, 167);
-            this.checkBoxPokazHaslo.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBoxPokazHaslo.Location = new System.Drawing.Point(564, 209);
+            this.checkBoxPokazHaslo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.checkBoxPokazHaslo.Name = "checkBoxPokazHaslo";
-            this.checkBoxPokazHaslo.Size = new System.Drawing.Size(106, 20);
+            this.checkBoxPokazHaslo.Size = new System.Drawing.Size(122, 24);
             this.checkBoxPokazHaslo.TabIndex = 7;
             this.checkBoxPokazHaslo.Text = "Pokaż hasło";
             this.checkBoxPokazHaslo.UseVisualStyleBackColor = true;
@@ -76,10 +78,10 @@
             // buttonZalogujPacjent
             // 
             this.buttonZalogujPacjent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.buttonZalogujPacjent.Location = new System.Drawing.Point(501, 104);
-            this.buttonZalogujPacjent.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonZalogujPacjent.Location = new System.Drawing.Point(564, 130);
+            this.buttonZalogujPacjent.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonZalogujPacjent.Name = "buttonZalogujPacjent";
-            this.buttonZalogujPacjent.Size = new System.Drawing.Size(143, 47);
+            this.buttonZalogujPacjent.Size = new System.Drawing.Size(161, 59);
             this.buttonZalogujPacjent.TabIndex = 8;
             this.buttonZalogujPacjent.Text = "Zaloguj";
             this.buttonZalogujPacjent.UseVisualStyleBackColor = true;
@@ -90,10 +92,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(220, 20);
+            this.label3.Location = new System.Drawing.Point(248, 25);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(289, 32);
+            this.label3.Size = new System.Drawing.Size(338, 38);
             this.label3.TabIndex = 9;
             this.label3.Text = "WITAMY W KLINICE";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -103,9 +105,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label2.Location = new System.Drawing.Point(46, 116);
+            this.label2.Location = new System.Drawing.Point(52, 145);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.Size = new System.Drawing.Size(95, 25);
             this.label2.TabIndex = 10;
             this.label2.Text = "E-MAIL:";
             // 
@@ -114,27 +116,27 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label1.Location = new System.Drawing.Point(46, 186);
+            this.label1.Location = new System.Drawing.Point(52, 232);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.Size = new System.Drawing.Size(95, 25);
             this.label1.TabIndex = 14;
             this.label1.Text = "HASŁO:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(408, 204);
+            this.label4.Location = new System.Drawing.Point(459, 255);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(173, 16);
+            this.label4.Size = new System.Drawing.Size(203, 20);
             this.label4.TabIndex = 15;
             this.label4.Text = "Nie możesz się zalogować?";
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(587, 203);
+            this.linkLabel1.Location = new System.Drawing.Point(660, 254);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(83, 16);
+            this.linkLabel1.Size = new System.Drawing.Size(96, 20);
             this.linkLabel1.TabIndex = 16;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Zmień hasło";
@@ -143,19 +145,19 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.Location = new System.Drawing.Point(203, 295);
+            this.label5.Location = new System.Drawing.Point(228, 369);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(306, 20);
+            this.label5.Size = new System.Drawing.Size(380, 25);
             this.label5.TabIndex = 17;
             this.label5.Text = "BRAK KONTA? ZAREJESTRUJ SIĘ";
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button2.Location = new System.Drawing.Point(275, 322);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(309, 402);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(167, 52);
+            this.button2.Size = new System.Drawing.Size(188, 65);
             this.button2.TabIndex = 18;
             this.button2.Text = "REJESTRACJA";
             this.button2.UseVisualStyleBackColor = true;
@@ -165,19 +167,19 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label6.Location = new System.Drawing.Point(203, 393);
+            this.label6.Location = new System.Drawing.Point(228, 491);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(313, 20);
+            this.label6.Size = new System.Drawing.Size(394, 25);
             this.label6.TabIndex = 19;
             this.label6.Text = "PONOWNY WYBÓR UŻYTKOWNIKA";
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button3.Location = new System.Drawing.Point(275, 419);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(309, 524);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(167, 47);
+            this.button3.Size = new System.Drawing.Size(188, 59);
             this.button3.TabIndex = 20;
             this.button3.Text = "<< COFNIJ";
             this.button3.UseVisualStyleBackColor = true;
@@ -185,10 +187,10 @@
             // 
             // FormLogowaniePacjent
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(713, 501);
+            this.ClientSize = new System.Drawing.Size(802, 626);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.button2);
@@ -202,7 +204,7 @@
             this.Controls.Add(this.checkBoxPokazHaslo);
             this.Controls.Add(this.textBoxHasloPacjent);
             this.Controls.Add(this.textBoxLoginPacjent);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FormLogowaniePacjent";
             this.Text = "Logowanie Pacjent";
             this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
